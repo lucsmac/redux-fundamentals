b13bcb4e00d430491d18ff9a09101ae11f6319a0
Lucas Macedo <lucasmdc57@gmail.com>
2021-04-07 09:28:00 -0300

    refactoring catalog

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,11 +1,9 @@
-import { useCallback, useEffect, useState } from "react"
-import { useDispatch } from "react-redux"
+import { useEffect, useState } from "react"
 import api from "../services/api"
-import { addProductToCart } from "../store/modules/cart/actions"
 import { IProduct } from "../store/modules/cart/types"
+import CatalogItem from "./CatalogItem"
 
 const Catalog: React.FC = () => {
-  const dispatch = useDispatch()
   const [catalog, setCatalog] = useState<IProduct[]>([])
 
   useEffect(() => {
@@ -13,27 +11,13 @@ const Catalog: React.FC = () => {
       setCatalog(response.data)
     })
   }, [])
-
-  const handleAddProductToCart = useCallback((product: IProduct) => {
-    dispatch(addProductToCart(product))
-  }, [dispatch])
   
   return (
     <main>
       <h1>Catalog</h1>
 
       {catalog.map(product => (
-        <article key={product.id}>
-          <strong>{product.title}</strong> {" - "}
-          <span>{product.price}</span> { "  "}
-
-          <button
-            type="button"
-            onClick={() => handleAddProductToCart(product)}
-          >
-            Comprar
-          </button>
-        </article>
+        <CatalogItem product={product} key={product.id} />
       ))}
     </main>
   )
