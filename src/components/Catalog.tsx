06535bf8a3dd9ab8a0cab196fe0d0d62bc4941a6
Lucas Macedo <lucasmdc57@gmail.com>
2021-04-06 23:04:42 -0300

    creating store

---
+++ src/components/Catalog.tsx
@@ -0,0 +1,13 @@
+import { useSelector } from "react-redux"
+
+const Catalog: React.FC = () => {
+  const catalog = useSelector(state => state)
+
+  console.log(catalog)
+  
+  return (<div>
+    <h1>Catalog</h1>
+  </div>)
+}
+
+export default Catalog
